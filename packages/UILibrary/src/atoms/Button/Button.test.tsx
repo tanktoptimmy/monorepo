3bc70d15914c5078612523d2c5868fe10872d7e4
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-03-12 07:41:41 +0000

    further setup including tests

---
+++ packages/UILibrary/src/atoms/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Button from "./Button"
+
+describe("button", ()=> {
+  it('renders correctly', () => {
+    render(<Button label="Example button"/>)
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  })
+})
---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import { Button } from "@tr.p/ui-library"
+
+ReactDOM.render(
+  <div>
+    <Button label="BOOM"/>
+  </div>,
+  document.querySelector('#root')
+)
