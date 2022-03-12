9068877e3381aeaff6ee25faef5bcc67db3faff9
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-03-12 11:34:24 +0000

    adding storybook

---
+++ packages/UILibrary/.storybook/main.js
@@ -0,0 +1,22 @@
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+      '@storybook/addon-storysource',
+      '@storybook/addon-knobs',
+      '@storybook/addon-a11y',
+      '@storybook/addon-essentials'
+  ],
+  framework: '@storybook/react',
+  babel: async (options) => ({
+    // Update your babel configuration here
+    ...options,
+  }),
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    // Return the altered config
+    return config;
+  }
+}
+
+
+
---
+++ packages/UILibrary/src/atoms/Button/Button.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Button'
+export default {
+    title: 'Atoms|Button'
+}
+
+export const Common = () => <Button label={"Trial and error"} />
+
+
+
+export const CustomLabel = () => <Button label='Select a color' />
